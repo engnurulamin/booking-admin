8bc1e41d81a7a828de54dce6d59ff1b47a9134d9
Nurul Amin <namin2750@gmail.com>
2024-01-08 17:00:07 +0600

    port fix

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -22,10 +22,8 @@ const Login = () => {
     e.preventDefault();
     dispatch({ type: "LOGIN_START" });
     try {
-      const res = await axios.post(
-        "http://localhost:8800/api/auth/login",
-        credentials
-      );
+      const apiUrl = "http://localhost:8800/api/auth/login";
+      const res = await axios.post(apiUrl, credentials);
       if (res.data.isAdmin) {
         dispatch({ type: "LOGIN_SUCCESS", payload: res.data.details });
         navigate("/");
