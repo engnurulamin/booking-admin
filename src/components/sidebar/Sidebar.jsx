939671aba4dd98523630a8ba7ae3d58a45310ec0
Nurul Amin <namin2750@gmail.com>
2024-01-15 21:19:12 +0600

    active tab

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import "./sidebar.scss";
 import DashboardIcon from "@mui/icons-material/Dashboard";
 import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
@@ -16,7 +16,13 @@ import { Link } from "react-router-dom";
 import { DarkModeContext } from "../../context/DarkModeContext.jsx";
 
 const Sidebar = () => {
+  const [active_tab, setActiveTab] = useState(1);
   const { dispatch } = useContext(DarkModeContext);
+
+  function activeTab(index) {
+    setActiveTab(index);
+    console.log(active_tab);
+  }
   return (
     <div className="sidebar">
       <div className="top">
@@ -31,62 +37,117 @@ const Sidebar = () => {
         <ul>
           <p className="title">MAIN</p>
           <Link to="/" style={{ textDecoration: "none" }}>
-            <li>
+            <li
+              className={active_tab === 1 ? "is-active" : ""}
+              onClick={() => {
+                activeTab(1);
+              }}
+            >
               <DashboardIcon className="icon" />
               <span>Dashboard</span>
             </li>
           </Link>
           <p className="title">LISTS</p>
           <Link to="/users" style={{ textDecoration: "none" }}>
-            <li>
+            <li
+              className={active_tab === 2 ? "is-active" : ""}
+              onClick={() => {
+                activeTab(2);
+              }}
+            >
               <PersonOutlineIcon className="icon" />
               <span>Users</span>
             </li>
           </Link>
           <Link to="/hotels" style={{ textDecoration: "none" }}>
-            <li>
+            <li
+              className={active_tab === 3 ? "is-active" : ""}
+              onClick={() => {
+                activeTab(3);
+              }}
+            >
               <StoreIcon className="icon" />
               <span>Hotels</span>
             </li>
           </Link>
           <Link to="/rooms" style={{ textDecoration: "none" }}>
-            <li>
+            <li
+              className={active_tab === 4 ? "is-active" : ""}
+              onClick={() => {
+                activeTab(4);
+              }}
+            >
               <CreditCardIcon className="icon" />
               <span>Rooms</span>
             </li>
           </Link>
-          <li>
+          <li
+            className={active_tab === 5 ? "is-active" : ""}
+            onClick={() => {
+              activeTab(6);
+            }}
+          >
             <LocalShippingIcon className="icon" />
             <span>Delivery</span>
           </li>
           <p className="title">USEFUL</p>
-          <li>
+          <li
+            className={active_tab === 6 ? "is-active" : ""}
+            onClick={() => {
+              activeTab(7);
+            }}
+          >
             <InsertChartIcon className="icon" />
             <span>Stats</span>
           </li>
-          <li>
+          <li
+            className={active_tab === 7 ? "is-active" : ""}
+            onClick={() => {
+              activeTab(8);
+            }}
+          >
             <NotificationsNoneIcon className="icon" />
             <span>Notifications</span>
           </li>
           <p className="title">SERVICE</p>
-          <li>
+          <li
+            className={active_tab === 8 ? "is-active" : ""}
+            onClick={() => {
+              activeTab(9);
+            }}
+          >
             <SettingsSystemDaydreamOutlinedIcon className="icon" />
             <span>System Health</span>
           </li>
-          <li>
+          <li className={active_tab === 9 ? "is-active" : ""}>
             <PsychologyOutlinedIcon className="icon" />
             <span>Logs</span>
           </li>
-          <li>
+          <li
+            className={active_tab === 10 ? "is-active" : ""}
+            onClick={() => {
+              activeTab(10);
+            }}
+          >
             <SettingsApplicationsIcon className="icon" />
             <span>Settings</span>
           </li>
           <p className="title">USER</p>
-          <li>
+          <li
+            className={active_tab === 11 ? "is-active" : ""}
+            onClick={() => {
+              activeTab(11);
+            }}
+          >
             <AccountCircleOutlinedIcon className="icon" />
             <span>Profile</span>
           </li>
-          <li>
+          <li
+            className={active_tab === 12 ? "is-active" : ""}
+            onClick={() => {
+              activeTab(12);
+            }}
+          >
             <ExitToAppIcon className="icon" />
             <span>Logout</span>
           </li>
