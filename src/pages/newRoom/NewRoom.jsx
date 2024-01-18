fd673fa68dcf6a5ad455039b8f186c7a13ae643c
Nurul Amin <namin2750@gmail.com>
2024-01-18 21:52:58 +0600

    add room

--- src/pages/newRoom/NewRoom.jsx
+++ src/pages/newRoom/NewRoom.jsx
@@ -5,15 +5,30 @@ import Navbar from "../../components/navbar/Navbar";
 import DriveFolderUploadOutlinedIcon from "@mui/icons-material/DriveFolderUploadOutlined";
 import useFetch from "../../hooks/useFetch";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const NewRoom = ({ inputs, title }) => {
   const [info, setInfo] = useState({});
   const [hotelId, sethotelId] = useState(undefined);
+  const [rooms, setRooms] = useState([]);
   const { data, loading, error } = useFetch("http://localhost:8800/api/hotels");
   const navigate = useNavigate();
 
   const handleChange = (e) => {
     setInfo((prev) => ({ ...prev, [e.target.id]: e.target.value }));
+  };
+  console.log(hotelId);
+  const handleClick = async (e) => {
+    e.preventDefault();
+    const roomNumbers = rooms.split(",").map((room) => ({ number: room }));
+    console.log(roomNumbers);
+    try {
+      const roomApiUrl = `http://localhost:8800/api/rooms/${hotelId}`;
+      await axios.post(roomApiUrl, { ...info, roomNumbers });
+      navigate("/rooms");
+    } catch (err) {
+      console.log(err);
+    }
   };
   return (
     <div className="new-room">
@@ -31,12 +46,21 @@ const NewRoom = ({ inputs, title }) => {
                   <label>{input.label}</label>
                   <input
                     onChange={handleChange}
-                    id={input.type}
+                    id={input.id}
                     type={input.type}
                     placeholder={input.placeholder}
                   />
                 </div>
               ))}
+              <div className="form-input">
+                <label>Rooms</label>
+                <textarea
+                  onChange={(e) => setRooms(e.target.value)}
+                  placeholder="Give comma between room numbers"
+                  name=""
+                  id=""
+                ></textarea>
+              </div>
               <div className="form-input">
                 <label>Choose a hotel</label>
                 <select
@@ -48,11 +72,13 @@ const NewRoom = ({ inputs, title }) => {
                     ? "Loading"
                     : data &&
                       data.map((hotel) => (
-                        <option value={hotel._id}>{hotel.name}</option>
+                        <option key={hotel._id} value={hotel._id}>
+                          {hotel.name}
+                        </option>
                       ))}
                 </select>
               </div>
-              <button>Send</button>
+              <button onClick={handleClick}>Send</button>
             </form>
           </div>
         </div>
